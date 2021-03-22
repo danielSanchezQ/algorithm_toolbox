076d151ce542b7be2e972ed2c9f78b480f28375a
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2021-03-22 23:04:54 +0100

    Implemented closest points (not working properly)

--- src/search.rs
+++ src/search.rs
@@ -1,7 +1,7 @@
 use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::hash::Hash;
-use std::ops::{Div, RangeInclusive};
+use std::ops::Div;
 
 pub fn binary_search<T>(slice: &[T], element: &T) -> Option<usize>
 where
@@ -93,6 +93,40 @@ pub fn points_on_segments(segments: &[(i64, i64)], points: &[i64]) -> Vec<u64> {
         .collect()
 }
 
+type Point = (i64, i64);
+
+fn distance_squared((x1, y1): Point, (x2, y2): Point) -> i64 {
+    let xx = x1 - x2;
+    let yy = y1 - y2;
+    xx * xx + yy * yy
+}
+/// points (x, y) are considered already sorted by their x coordinate
+fn closest_points_split(points: &[Point]) -> f64 {
+    match points {
+        &[] | &[_] => f64::MAX,
+        &[p1, p2] => distance_squared(p1, p2) as f64,
+        points => {
+            let pivot = points.len() / 2;
+            let left_min = closest_points_split(&points[..pivot]);
+            let right_min = closest_points_split(&points[pivot..]);
+            left_min.min(right_min)
+        }
+    }
+}
+
+fn closest_points(points: &mut [Point]) -> f64 {
+    points.sort_by_key(|p| p.0);
+    let min_x = closest_points_split(&points);
+    let pivot = (points.len() / 2) as i64;
+    let mut points_by_y: Vec<Point> = points
+        .iter()
+        .filter(|p| (pivot - p.0.abs()) as f64 <= min_x)
+        .cloned()
+        .collect();
+    points_by_y.sort_by_key(|p| p.1);
+    min_x.min(closest_points_split(&points_by_y)).sqrt()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -153,5 +187,17 @@ mod test {
         let points = [1, 6];
         let output = [2, 0];
         assert_eq!(&points_on_segments(&segments, &points), &output)
+    }
+
+    #[test]
+    fn closes_points_example() {
+        let mut points = [(0, 0), (3, 4)];
+        assert_eq!(closest_points(&mut points), 5f64)
+    }
+
+    #[test]
+    fn closes_points_example2() {
+        let mut points = [(7, 7), (1, 100), (4, 8), (7, 7)];
+        assert_eq!(closest_points(&mut points), 0f64)
     }
 }
