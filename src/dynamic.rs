cd606efafe50a730d6b55b9265ab2dc90cc6981d
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2021-03-28 17:54:26 +0200

    Proper impl of primitive_calculator

--- src/dynamic.rs
+++ src/dynamic.rs
@@ -17,37 +17,62 @@ pub fn minimum_coins_exchange(value: usize, coins: &[usize]) -> usize {
     slots[value]
 }
 
-pub fn primitive_calculator(
-    value: usize,
-    operations: &mut [impl FnMut(usize) -> usize],
-) -> (usize, Vec<usize>) {
-    let mut res = vec![1];
-    let mut init_i = 0;
-    let mut counter = 0;
-    'outer: loop {
-        let next_init_i = res.len();
-
-        for i in init_i..res.len() {
-            for op in operations.iter_mut() {
-                let op_res = op(res[i]);
-                if op_res <= value {
-                    res.push(op_res);
-                }
-                if op_res == value {
-                    counter += 1;
-                    break 'outer;
+pub fn primitive_calculator(value: usize) -> Vec<usize> {
+    let mut min_steps = vec![0usize; value + 1];
+    let mut predecessor = vec![0usize; value + 1];
+    for i in 2..=value {
+        min_steps[i] = min_steps[i - 1] + 1;
+        predecessor[i] = i - 1;
+        for n in 2..=3 {
+            if i % n == 0 {
+                let i_div_n = i / n;
+                if min_steps[i_div_n] < min_steps[i] {
+                    min_steps[i] = min_steps[i_div_n] + 1;
+                    predecessor[i] = i_div_n;
                 }
             }
         }
-        counter += 1;
-        init_i = next_init_i;
     }
-    (counter, res)
+    let mut res = Vec::new();
+    let mut i = value;
+    while i != 0 {
+        res.push(i);
+        i = predecessor[i];
+    }
+    res.reverse();
+    res
+}
+
+pub fn edit_distance(s1: &str, s2: &str) -> usize {
+    let s1: Vec<_> = s1.chars().collect();
+    let s2: Vec<_> = s2.chars().collect();
+    let n = s1.len() + 1;
+    let m = s2.len() + 1;
+    let mut d: Vec<Vec<usize>> = vec![vec![0usize; m]; n];
+    // empty strings
+    d[0] = (0..m).collect();
+    for (i, item) in d.iter_mut().enumerate() {
+        item[0] = i;
+    }
+    for j in 1..m {
+        for i in 1..n {
+            let insertion = d[i][j - 1] + 1;
+            let deletion = d[i - 1][j] + 1;
+            let _match = d[i - 1][j - 1];
+            let mismatch = d[i - 1][j - 1] + 1;
+            if s1[i - 1] == s2[j - 1] {
+                d[i][j] = *[insertion, deletion, _match].iter().min().unwrap();
+            } else {
+                d[i][j] = *[insertion, deletion, mismatch].iter().min().unwrap();
+            }
+        }
+    }
+    d[s1.len()][s2.len()]
 }
 
 #[cfg(test)]
 mod test {
-    use crate::dynamic::{minimum_coins_exchange, primitive_calculator};
+    use crate::dynamic::{edit_distance, minimum_coins_exchange, primitive_calculator};
 
     #[test]
     fn test_exchange_example() {
@@ -55,10 +80,23 @@ mod test {
     }
 
     #[test]
-    fn test_seq() {
-        let mut ops = [|v| v + 1, |v| v * 2, |v| v * 3];
-        let res = primitive_calculator(10, &mut ops);
+    fn test_primitive_calculator_example() {
+        let res = primitive_calculator(5);
+        assert_eq!(res.as_slice(), [1, 2, 4, 5]);
+    }
 
-        println!("{:?}", res);
+    #[test]
+    fn test_edit_distance_example() {
+        assert_eq!(edit_distance("ab", "ab"), 0);
+    }
+
+    #[test]
+    fn test_edit_distance_example2() {
+        assert_eq!(edit_distance("ports", "shorts"), 3);
+    }
+
+    #[test]
+    fn test_edit_distance_example3() {
+        assert_eq!(edit_distance("editing", "distance"), 5);
     }
 }
